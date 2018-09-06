d85606fd0fb2a2cf0ee70e0dbc885ef03e1e6ffe
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2018-09-06 17:29:01 +0000

    Refactor

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -66,236 +65,22 @@ func main() {
 		log.Fatalf("can't discover profile: %s", err)
 	}
 
-	deviceName(p, cln)
-	firmwareRevision(p, cln)
-	enableIRTemperature(p, cln)
-	isEnableIRTemperature(p, cln)
-	irTemperature(p, cln)
+	s := NewSensorTag(p, cln)
+	s.DeviceName()
+	s.FirmwareRevision()
+	s.EnableIRTemperature()
+	s.IsEnableIRTemperature()
+	s.IRTemperature()
 	time.Sleep(time.Second)
-	irTemperature(p, cln)
+	s.IRTemperature()
 	time.Sleep(time.Second)
-	irTemperature(p, cln)
+	s.IRTemperature()
 	time.Sleep(time.Second)
-	irTemperature(p, cln)
+	s.IRTemperature()
 
 	// Disconnect the connection. (On OS X, this might take a while.)
 	fmt.Printf("Disconnecting [ %s ]... (this might take up to few seconds on OS X)\n", cln.Address())
 	cln.CancelConnection()
 
 	<-done
-}
-
-func deviceName(profile *ble.Profile, client ble.Client) (string, error) {
-	resp, err := readCharacteristics(profile, client, "1800", "2a00")
-	s := string(resp)
-	log.Print("DeviceName")
-	log.Print("[read ] ", s)
-	return s, err
-}
-
-func firmwareRevision(profile *ble.Profile, client ble.Client) (string, error) {
-	resp, err := readCharacteristics(profile, client, "180a", "2a26")
-	s := string(resp)
-	log.Print("Firmware Revision")
-	log.Print("[read ] ", s)
-	return s, err
-}
-
-func irTemperature(profile *ble.Profile, client ble.Client) ([]byte, error) {
-	resp, err := readCharacteristics(profile,
-		client,
-		"f000aa0004514000b000000000000000",
-		"f000aa0104514000b000000000000000")
-	log.Print("IR Temperature")
-	log.Print("[read ] ", resp)
-	return resp, err
-}
-
-func enableIRTemperature(profile *ble.Profile, client ble.Client) error {
-	log.Print("enable IR Temperature")
-	return writeIRTemperature(profile, client, []byte{1, 0})
-}
-
-func disableIRTemperature(profile *ble.Profile, client ble.Client) error {
-	log.Print("disable IR Temperature")
-	return writeIRTemperature(profile, client, []byte{0, 0})
-}
-
-func isEnableIRTemperature(profile *ble.Profile, client ble.Client) (bool, error) {
-	value, err := readDescriptor(profile,
-		client,
-		"f000aa0004514000b000000000000000",
-		"f000aa0104514000b000000000000000",
-		"2902")
-	log.Print("is enable IR Temperature?")
-	log.Print("[read ] ", value)
-	return true, err
-}
-
-func writeIRTemperature(profile *ble.Profile, client ble.Client, value []byte) error {
-	err := writeDescriptor(profile,
-		client,
-		"f000aa0004514000b000000000000000",
-		"f000aa0104514000b000000000000000",
-		"2902",
-		value)
-	log.Print("[write] ", value)
-
-	return err
-}
-
-func readCharacteristics(profile *ble.Profile,
-	client ble.Client,
-	service string,
-	characteristics string) ([]byte, error) {
-	sUUID, err := ble.Parse(service)
-	if err != nil {
-		log.Print("err: ", err)
-		return nil, err
-	}
-	cUUID, err := ble.Parse(characteristics)
-	if err != nil {
-		log.Print("err: ", err)
-		return nil, err
-	}
-	c, err := getCharacteristics(profile, sUUID, cUUID)
-	if err != nil {
-		log.Print("err: ", err)
-		return nil, err
-	}
-
-	buf, err := client.ReadCharacteristic(c)
-	if err != nil {
-		log.Print("err: ", err)
-		return nil, err
-	}
-	return buf, nil
-}
-
-func writeCharacteristics(profile *ble.Profile,
-	client ble.Client,
-	service string,
-	characteristics string,
-	value []byte) error {
-	sUUID, err := ble.Parse(service)
-	if err != nil {
-		log.Print("err: ", err)
-		return err
-	}
-	cUUID, err := ble.Parse(characteristics)
-	if err != nil {
-		log.Print("err: ", err)
-		return err
-	}
-	c, err := getCharacteristics(profile, sUUID, cUUID)
-	if err != nil {
-		log.Print("err: ", err)
-		return err
-	}
-
-	err = client.WriteCharacteristic(c, value, false)
-	if err != nil {
-		log.Print("err: ", err)
-		return err
-	}
-	return nil
-}
-
-func writeDescriptor(profile *ble.Profile,
-	client ble.Client,
-	service string,
-	characteristics string,
-	descriptor string,
-	value []byte) error {
-	sUUID, err := ble.Parse(service)
-	if err != nil {
-		log.Print("err: ", err)
-		return err
-	}
-	cUUID, err := ble.Parse(characteristics)
-	if err != nil {
-		log.Print("err: ", err)
-		return err
-	}
-	dUUID, err := ble.Parse(descriptor)
-	if err != nil {
-		log.Print("err: ", err)
-		return err
-	}
-	d, err := getDescriptor(profile, sUUID, cUUID, dUUID)
-	if err != nil {
-		log.Print("err: ", err)
-		return err
-	}
-
-	err = client.WriteDescriptor(d, value)
-	if err != nil {
-		log.Print("err: ", err)
-		return err
-	}
-	return nil
-}
-
-func readDescriptor(profile *ble.Profile,
-	client ble.Client,
-	service string,
-	characteristics string,
-	descriptor string) ([]byte, error) {
-	sUUID, err := ble.Parse(service)
-	if err != nil {
-		log.Print("err: ", err)
-		return nil, err
-	}
-	cUUID, err := ble.Parse(characteristics)
-	if err != nil {
-		log.Print("err: ", err)
-		return nil, err
-	}
-	dUUID, err := ble.Parse(descriptor)
-	if err != nil {
-		log.Print("err: ", err)
-		return nil, err
-	}
-	d, err := getDescriptor(profile, sUUID, cUUID, dUUID)
-	if err != nil {
-		log.Print("err: ", err)
-		return nil, err
-	}
-
-	value, err := client.ReadDescriptor(d)
-	if err != nil {
-		log.Print("err: ", err)
-		return nil, err
-	}
-	return value, nil
-}
-
-func getCharacteristics(profile *ble.Profile, service []byte, characteristics []byte) (*ble.Characteristic, error) {
-	for _, s := range profile.Services {
-		if !s.UUID.Equal(service) {
-			continue
-		}
-		for _, c := range s.Characteristics {
-			if !c.UUID.Equal(characteristics) {
-				continue
-			}
-			return c, nil
-		}
-	}
-	return nil, errors.New("not found Characteristic")
-}
-
-func getDescriptor(profile *ble.Profile, service []byte, characteristics []byte, descriptor []byte) (*ble.Descriptor, error) {
-	c, err := getCharacteristics(profile, service, characteristics)
-	if err != nil {
-		return nil, err
-	}
-
-	for _, d := range c.Descriptors {
-		if !d.UUID.Equal(descriptor) {
-			continue
-		}
-		return d, nil
-	}
-	return nil, errors.New("not found Descriptor")
 }
---
+++ sensor_tag.go
@@ -0,0 +1,232 @@
+package main
+
+import (
+	"errors"
+	"log"
+
+	"github.com/currantlabs/ble"
+)
+
+type SensorTag struct {
+	profile *ble.Profile
+	client  ble.Client
+}
+
+func NewSensorTag(profile *ble.Profile, client ble.Client) *SensorTag {
+	return &SensorTag{profile: profile, client: client}
+}
+
+func (s *SensorTag) DeviceName() (string, error) {
+	resp, err := readCharacteristics(s.profile, s.client, "1800", "2a00")
+	value := string(resp)
+	log.Print("DeviceName")
+	log.Print("[read ] ", value)
+	return string(value), err
+}
+
+func (s *SensorTag) FirmwareRevision() (string, error) {
+	resp, err := readCharacteristics(s.profile, s.client, "180a", "2a26")
+	value := string(resp)
+	log.Print("Firmware Revision")
+	log.Print("[read ] ", value)
+	return string(value), err
+}
+
+func (s *SensorTag) IRTemperature() ([]byte, error) {
+	resp, err := readCharacteristics(s.profile,
+		s.client,
+		"f000aa0004514000b000000000000000",
+		"f000aa0104514000b000000000000000")
+	log.Print("IR Temperature")
+	log.Print("[read ] ", resp)
+	return resp, err
+}
+
+func (s *SensorTag) EnableIRTemperature() error {
+	log.Print("enable IR Temperature")
+	return s.writeIRTemperature([]byte{1, 0})
+}
+
+func (s *SensorTag) DisableIRTemperature() error {
+	log.Print("disable IR Temperature")
+	return s.writeIRTemperature([]byte{0, 0})
+}
+
+func (s *SensorTag) IsEnableIRTemperature() (bool, error) {
+	value, err := readDescriptor(s.profile,
+		s.client,
+		"f000aa0004514000b000000000000000",
+		"f000aa0104514000b000000000000000",
+		"2902")
+	log.Print("is enable IR Temperature?")
+	log.Print("[read ] ", value)
+	return true, err
+}
+
+func (s *SensorTag) writeIRTemperature(value []byte) error {
+	err := writeDescriptor(s.profile,
+		s.client,
+		"f000aa0004514000b000000000000000",
+		"f000aa0104514000b000000000000000",
+		"2902",
+		value)
+	log.Print("[write] ", value)
+
+	return err
+}
+
+func readCharacteristics(profile *ble.Profile,
+	client ble.Client,
+	service string,
+	characteristics string) ([]byte, error) {
+	sUUID, err := ble.Parse(service)
+	if err != nil {
+		log.Print("err: ", err)
+		return nil, err
+	}
+	cUUID, err := ble.Parse(characteristics)
+	if err != nil {
+		log.Print("err: ", err)
+		return nil, err
+	}
+	c, err := getCharacteristics(profile, sUUID, cUUID)
+	if err != nil {
+		log.Print("err: ", err)
+		return nil, err
+	}
+
+	buf, err := client.ReadCharacteristic(c)
+	if err != nil {
+		log.Print("err: ", err)
+		return nil, err
+	}
+	return buf, nil
+}
+
+func writeCharacteristics(profile *ble.Profile,
+	client ble.Client,
+	service string,
+	characteristics string,
+	value []byte) error {
+	sUUID, err := ble.Parse(service)
+	if err != nil {
+		log.Print("err: ", err)
+		return err
+	}
+	cUUID, err := ble.Parse(characteristics)
+	if err != nil {
+		log.Print("err: ", err)
+		return err
+	}
+	c, err := getCharacteristics(profile, sUUID, cUUID)
+	if err != nil {
+		log.Print("err: ", err)
+		return err
+	}
+
+	err = client.WriteCharacteristic(c, value, false)
+	if err != nil {
+		log.Print("err: ", err)
+		return err
+	}
+	return nil
+}
+
+func writeDescriptor(profile *ble.Profile,
+	client ble.Client,
+	service string,
+	characteristics string,
+	descriptor string,
+	value []byte) error {
+	sUUID, err := ble.Parse(service)
+	if err != nil {
+		log.Print("err: ", err)
+		return err
+	}
+	cUUID, err := ble.Parse(characteristics)
+	if err != nil {
+		log.Print("err: ", err)
+		return err
+	}
+	dUUID, err := ble.Parse(descriptor)
+	if err != nil {
+		log.Print("err: ", err)
+		return err
+	}
+	d, err := getDescriptor(profile, sUUID, cUUID, dUUID)
+	if err != nil {
+		log.Print("err: ", err)
+		return err
+	}
+
+	err = client.WriteDescriptor(d, value)
+	if err != nil {
+		log.Print("err: ", err)
+		return err
+	}
+	return nil
+}
+
+func readDescriptor(profile *ble.Profile,
+	client ble.Client,
+	service string,
+	characteristics string,
+	descriptor string) ([]byte, error) {
+	sUUID, err := ble.Parse(service)
+	if err != nil {
+		log.Print("err: ", err)
+		return nil, err
+	}
+	cUUID, err := ble.Parse(characteristics)
+	if err != nil {
+		log.Print("err: ", err)
+		return nil, err
+	}
+	dUUID, err := ble.Parse(descriptor)
+	if err != nil {
+		log.Print("err: ", err)
+		return nil, err
+	}
+	d, err := getDescriptor(profile, sUUID, cUUID, dUUID)
+	if err != nil {
+		log.Print("err: ", err)
+		return nil, err
+	}
+
+	value, err := client.ReadDescriptor(d)
+	if err != nil {
+		log.Print("err: ", err)
+		return nil, err
+	}
+	return value, nil
+}
+
+func getCharacteristics(profile *ble.Profile, service []byte, characteristics []byte) (*ble.Characteristic, error) {
+	for _, s := range profile.Services {
+		if !s.UUID.Equal(service) {
+			continue
+		}
+		for _, c := range s.Characteristics {
+			if !c.UUID.Equal(characteristics) {
+				continue
+			}
+			return c, nil
+		}
+	}
+	return nil, errors.New("not found Characteristic")
+}
+
+func getDescriptor(profile *ble.Profile, service []byte, characteristics []byte, descriptor []byte) (*ble.Descriptor, error) {
+	c, err := getCharacteristics(profile, service, characteristics)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, d := range c.Descriptors {
+		if !d.UUID.Equal(descriptor) {
+			continue
+		}
+		return d, nil
+	}
+	return nil, errors.New("not found Descriptor")
+}
